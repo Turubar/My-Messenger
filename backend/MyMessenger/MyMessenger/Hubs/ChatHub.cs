1898d2ebac1b58b379a53d832c228682bfacd23f
Turubar <ivan.turubar@gmail.com>
2024-12-07 19:15:33 +0300

    Создание backend проекта

---
+++ backend/MyMessenger/MyMessenger/Hubs/ChatHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace MyMessenger.Hubs
+{
+    public interface IChatClient
+    {
+        public Task ReceiveMessage(string userName, string message);
+    }
+
+    public class ChatHub : Hub<IChatClient>
+    {
+        public async Task JoinChat(UserConnection connection)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, connection.ChatRoom);
+
+            await Clients
+                .Group(connection.ChatRoom)
+                .ReceiveMessage("Admin", $"{connection.UserName} присоединился к чату");
+        }
+    }
+
+}
---
+++ backend/MyMessenger/MyMessenger/Models/UserConnection.cs
@@ -0,0 +1,1 @@
+﻿public record UserConnection(string UserName, string ChatRoom);
