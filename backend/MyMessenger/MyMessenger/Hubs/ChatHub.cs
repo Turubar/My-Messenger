83c97dcbe9ae0d51a2f1b8cce071b5a5b5a675cc
Turubar <ivan.turubar@gmail.com>
2024-12-07 23:29:54 +0300

    Добавление кэширования с помощью Redis, создание docker compose

--- backend/MyMessenger/MyMessenger/Hubs/ChatHub.cs
+++ backend/MyMessenger/MyMessenger/Hubs/ChatHub.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
 
 namespace MyMessenger.Hubs
 {
@@ -9,14 +11,38 @@ namespace MyMessenger.Hubs
 
     public class ChatHub : Hub<IChatClient>
     {
+        private readonly IDistributedCache _cache;
+
+        public ChatHub(IDistributedCache cache) 
+        {
+            _cache = cache;
+        }
+
         public async Task JoinChat(UserConnection connection)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, connection.ChatRoom);
+
+            var stringConnection = JsonSerializer.Serialize(connection);
+
+            await _cache.SetStringAsync(Context.ConnectionId, stringConnection);
 
             await Clients
                 .Group(connection.ChatRoom)
                 .ReceiveMessage("Admin", $"{connection.UserName} присоединился к чату");
         }
-    }
 
+        public async Task SendMessage(string message)
+        {
+            var stringConnection = await _cache.GetAsync(Context.ConnectionId);
+
+            var connection = JsonSerializer.Deserialize<UserConnection>(stringConnection);
+
+            if (connection is not null)
+            {
+                await Clients
+                .Group(connection.ChatRoom)
+                .ReceiveMessage(connection.UserName, message);
+            }
+        }
+    }
 }
