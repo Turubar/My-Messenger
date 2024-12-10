6ec5b24548187c96d7522ed7be389c6338c6d2f8
Turubar <ivan.turubar@gmail.com>
2024-12-10 16:13:31 +0300

    Настройка маршрутизации

--- frontend/my-messenger-frontend/src/App.jsx
+++ frontend/my-messenger-frontend/src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css'
-import { Button } from '@mui/material'
+import { Route, Routes } from 'react-router-dom';
+import { HomePage, LoginPage, ProfilePage, RegisterPage, ErrorPage} from './components/pages'
+import PrivateRoute from './utils/router/privateRoute';
 
 function App() {
-
   return (
-    <>
-      <Button variant="contained">Привет</Button>
-    </>
-  )
+      <Routes>
+        <Route path="register" element={<RegisterPage />} />
+        <Route path="login" element={<LoginPage />} />
+
+        <Route element={<PrivateRoute />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="profile" element={<ProfilePage />} />
+        </Route>
+
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+  );
 }
 
-export default App
+export default App;
