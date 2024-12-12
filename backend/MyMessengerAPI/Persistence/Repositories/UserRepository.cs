01786435f66409ed49b043929d73c5da9d2a2891
Turubar <ivan.turubar@gmail.com>
2024-12-12 12:43:23 +0300

    Создание application layer
    Определение интерфейса для работы с UserRepository

---
+++ backend/MyMessengerAPI/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using Application.Interfaces.Repositories;
+using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public Task Add(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetByLogin(string login)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
